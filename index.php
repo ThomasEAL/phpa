9feaa0a7f01f222610e106aef6c61430e740d9d2
Thomas Jessen <tbj@eal.dk>
2018-08-31 10:50:31 +0200

    tilføjet title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Min forside</title>
     </head>
     <body>
         <form action="login.php" method="post">
